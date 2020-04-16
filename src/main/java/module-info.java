ab29b02d85c076098aee2fe1e38fe091a9d6a0a5
iaroslavua <i.muliak@gmail.com>
2020-04-16 15:25:30 +0300

    Java 11 modules migration;

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module orderbook {
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.web;
+    requires org.slf4j;
+    requires spring.beans;
+    requires reactor.core;
+    requires com.fasterxml.classmate;
+}
