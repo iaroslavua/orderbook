2f0af0c6026bb45abb8f435659b16258f4ba0be6
iaroslavua <i.muliak@gmail.com>
2020-04-16 15:26:21 +0300

    Imports organized

--- src/main/java/com/bit/orderbook/OrderbookApplication.java
+++ src/main/java/com/bit/orderbook/OrderbookApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderbookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrderbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OrderbookApplication.class, args);
+    }
 
 }
--- src/main/java/com/bit/orderbook/controllers/BinanceController.java
+++ src/main/java/com/bit/orderbook/controllers/BinanceController.java
@@ -5,13 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 
 @RestController
 @RequestMapping(path = "/order-book")
